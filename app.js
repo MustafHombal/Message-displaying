65819ef771960f4d381ddaa7d1b5f49921c9ab15
MustafHombal <135491164+MustafHombal@users.noreply.github.com>
2023-07-12 17:25:06 +0530

    first commit

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
+  //Get the form and input element
 const form = document.getElementById('message-form')
 const input = document.getElementById('input')
 
+  // Add event listener for form submission
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
   const message = input.value;
-
+  // Display an error message if the input is empty
   if(message.trim() === ''){
       const errorMsg = document.querySelector('.errorMsg')
       errorMsg.style.display = "block";
-      return;
+      return; // Stop further execution
   }
+          
+  // Hide the error message
   const errorMsg = document.querySelector('.errorMsg');
   errorMsg.style.display = 'none';
 
+  // Display the entered message in the result section
   const result = document.querySelector('.result')
   result.textContent = message;
   result.style.display = "block";
 
-
+  // Clear the input field
   input.value = '';
 })
